da90e13c98ebb5453bdbdfd5ebcf1605823702a3
AxelMasache <axeldaniel_02@hotmail.com>
2026-08-22 13:13:36 -0500

    funcionalidad Maven y logica de Torneo

--- src/puce/edu/ec/LigaPro.java
+++ src/puce/edu/ec/LigaPro.java
@@ -8,42 +8,65 @@ public class LigaPro {
          * 2 = DQuito
          */
 
-        // Matriz para registrar los enfrentamientos (4 equipos máximo)
-        int[][] partidosJugados = new int[3][3];
-
-        /* LDU (0) VS BSC (1) */
-        partidosJugados[0][1] = 1;
-        partidosJugados[1][0] = 1;
-
-        /* LDU (0) VS DQuito (2) */
-        partidosJugados[2][0] = 1;
-        partidosJugados[0][2] = 1;
-
-        /* BSC (1) VS DQuito (2) */
-        partidosJugados[1][2] = 1;
-        partidosJugados[2][1] = 1;
-
-        // Arreglo con los nombres exactos de los 3 equipos
         String[] equipos = {"LDU", "BSC", "DQuito"};
+        int totalEquipos = equipos.length;
 
-        // Imprimir cabecera de equipos
-        System.out.println("=== EQUIPOS REGISTRADOS ===");
-        for (int i = 0; i < equipos.length; i++) {
-            System.out.print(equipos[i] + "\t");
-        }
-        System.out.println("\n");
+        // Matriz de resultados (1 si jugaron, 0 si no)
+        int[][] partidosJugados = new int[totalEquipos][totalEquipos];
 
-        // Opcional: Mostrar la matriz de enfrentamientos de forma visual
-        System.       out.println("=== MATRIZ DE PARTIDOS ===" );
-        System.out.println("\tLDU\tBSC\tDQuito");
-        for (int i = 0; i < partidosJugados.length; i++) {
-            System.out.print(equipos[i] + "\t");
-            for (int j = 0; j < partidosJugados[i].length; j++) {
-                System.out.print(partidosJugados[i][j] + "\t");
+        /* Definimos algunos enfrentamientos y resultados simulados */
+        // LDU (0) vs BSC (1): LDU gana
+        partidosJugados[0][1] = 3; // Goles o puntos directos de LDU contra BSC
+        partidosJugados[1][0] = 0; // BSC contra LDU
+
+        // LDU (0) vs DQuito (2): Empate
+        partidosJugados[0][2] = 1;
+        partidosJugados[2][0] = 1;
+
+        // BSC (1) vs DQuito (2): BSC gana
+        partidosJugados[1][2] = 3;
+        partidosJugados[2][1] = 0;
+
+        // Arreglos para la tabla de posiciones: [Puntos, Partidos Jugados, Ganados, Perdidos, Empatados]
+        int[] puntos = new int[totalEquipos];
+        int[] jugados = new int[totalEquipos];
+        int[] ganados = new int[totalEquipos];
+        int[] perdidos = new int[totalEquipos];
+        int[] empatados = new int[totalEquipos];
+
+        // Procesar la lógica del torneo recorriendo la matriz
+        for (int i = 0; i < totalEquipos; i++) {
+            for (int j = 0; j < totalEquipos; j++) {
+                if (i != j && partidosJugados[i][j] > 0) {
+                    jugados[i]++;
+                    if (partidosJugados[i][j] == 3) {
+                        ganados[i]++;
+                        puntos[i] += 3;
+                    } else if (partidosJugados[i][j] == 1) {
+                        empatados[i]++;
+                        puntos[i] += 1;
+                    } else {
+                        perdidos[i]++;
+                    }
+                }
             }
-            System.out.println();
         }
+
+        // Mostrar la Tabla de Posiciones
+        System.out.println("=========================================");
+        System.out.println("          TABLA DE POSICIONES LIGAPRO    ");
+        System.out.println("=========================================");
+        System.out.println("Equipo\tPJ\tPG\tPE\tPP\tPTS");
+        System.out.println("-----------------------------------------");
+
+        for (int i = 0; i < totalEquipos; i++) {
+            System.out.println(equipos[i] + "\t" +
+                    jugados[i] + "\t" +
+                    ganados[i] + "\t" +
+                    empatados[i] + "\t" +
+                    perdidos[i] + "\t" +
+                    puntos[i]);
+        }
+        System.out.println("=========================================");
     }
 }
-
-
